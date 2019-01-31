b1974ff750ddf802a7cce9350429aa3cc74ff245
Marcin Czyż <marcim94@gmail.com>
2019-01-31 11:07:22 +0100

    Add SQL exercises

---
+++ SQLExercises/4.sql
@@ -0,0 +1,34 @@
+-- 'date' field should be considered to be escaped or renamed due to use of reserved key of many SQL databases
+
+-- MySQL
+SELECT
+  b.title,
+  count(r.rent_id) as number_of_rents_in_range
+FROM book b
+LEFT JOIN rent r ON
+  b.book_id = r.book_id AND
+  YEAR(r.date) = '2008'
+GROUP BY b.book_id;
+
+-- SQL Standard
+SELECT
+  b.title,
+  count(r.rent_id) as number_of_rents_in_range
+FROM book b
+LEFT JOIN rent r ON
+  b.book_id = r.book_id AND
+  r.date >= '2008-01-01' AND
+  r.date <= '2008-12-31'
+GROUP BY b.book_id;
+
+-- Sub-query version based on SQL Standard, use in case of possible optimisation when joining big amount of data
+SELECT
+	b.title,
+    (
+    	SELECT count(r.rent_id) FROM rent r
+        WHERE
+          r.book_id = b.book_id AND
+          r.date >= '2008-01-01' AND
+          r.date <= '2008-12-31'
+    ) AS number_of_rents_in_range
+FROM book b;
