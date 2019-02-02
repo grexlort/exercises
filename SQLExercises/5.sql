8d368ca301d413dc5aba1ec7eb02ba900e8c9908
Marcin Czyż <marcim94@gmail.com>
2019-02-02 22:58:13 +0100

    Escape table name

--- SQLExercises/5.sql
+++ SQLExercises/5.sql
@@ -1,5 +1,5 @@
 SELECT u.*
-FROM system_user u
+FROM `system_user` u
 LEFT JOIN system_user_has_atribute ua ON u.system_user_id = ua.system_user_id
 GROUP BY u.system_user_id
 HAVING count(ua.system_user_id) = 3;
